733a320730c2006f6b6f88afe3c95047f977b2a7
Facundo <vfacu@hotmail.com>
2024-10-11 22:24:35 -0300

    Agrego Modularización al codigo

---
+++ historial.py
@@ -0,0 +1,5 @@
+def mostrar_historial(historial):
+    print("\nHistorial de operaciones:")
+    for oper in historial:        
+        print(oper)
+    print() 
--- main.py
+++ main.py
@@ -1,51 +1,54 @@
 import display
+from operaciones import suma, resta, multiplicacion, division
+from historial import mostrar_historial
 
 def main():
     historial = []
     display.clear()
     choice = '1'    
-    while True:      
-        choice = input("Ingrese 'h' si quiere ver el Historial, sino presiones Inter para seguir ") 
+    while True: 
+        choice = input('Ingrese la operación (+, -, *, /, h(historial)) o "0" para salir:  ')
+
         if choice == "h":
-            print("\nHistorial de operaciones:")
-            for operacion in historial:
-                print(operacion)
-            print() 
+            mostrar_historial(historial)
+            continue 
+
+        if choice == "0":
+            print("Salir")
             break
-        else:
-            choice = input('Ingrese la operación (+, -, *, /, h(historial)) o "0" para salir:  ')
-            num1 = float(input("Ingrese un numero: "))  
-            num2 = float(input("Ingrese el segundo numero: "))            
-        
-            if choice == '+':
-                resultado = num1 + num2
-                operacion = f"{num1} + {num2} = {resultado}"
+
+        num1 = float(input("Ingrese un numero: "))  
+        num2 = float(input("Ingrese el segundo numero: "))  
+
+        if choice == '+':
+            resultado = suma(num1, num2)
+            operacion = f"{num1} + {num2} = {resultado}"
+            historial.append(operacion) 
+            print("La suma es:", operacion)
+        elif choice == '-':
+            resultado = resta(num1, num2)
+            operacion = f"{num1} - {num2} = {resultado}"
+            historial.append(operacion) 
+            print("La resta es:", operacion)
+        elif choice == '*':
+            resultado = multiplicacion(num1, num2)
+            operacion = f"{num1} * {num2} = {resultado}"
+            historial.append(operacion) 
+            print("La multiplicación es:", operacion)
+        elif choice == '/':
+            if num2 != 0:  
+                resultado = division(num1, num2)
+                operacion = f"{num1} / {num2} = {resultado}"
                 historial.append(operacion) 
-                print("La suma es:", operacion)
-            elif choice == '-':
-                resultado = num1 - num2
-                operacion = f"{num1} - {num2} = {resultado}"
-                historial.append(operacion) 
-                print("La resta es:", operacion)
-            elif choice == '*':
-                resultado = num1 * num2
-                operacion = f"{num1} * {num2} = {resultado}"
-                historial.append(operacion) 
-                print("La multiplicación es:", operacion)
-            elif choice == '/':
-                if num2 != 0:  
-                    resultado = num1 / num2
-                    operacion = f"{num1} / {num2} = {resultado}"
-                    historial.append(operacion) 
-                    print("La división es:", operacion)
-                else:
-                    print("Error: No se puede dividir entre cero.")      
-            elif choice == "0":            
-                print('Salir')
-                break        
+                print("La división es:", operacion)
             else:
-                print('Opción inválida, vuelva a intentarlo')
-        
-     
+                print("Error: No se puede dividir entre cero.")      
+        elif choice == "0":            
+            print('Salir')
+            break        
+        else:
+            print('Opción inválida, vuelva a intentarlo')  
+
+             
 if __name__ == "__main__":
     main()
