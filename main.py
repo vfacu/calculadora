7dd4b11399da8ded8f34eb1657077c22565d5e05
Facundo <vfacu@hotmail.com>
2024-10-13 21:13:32 -0300

    Se incorporo Manejo de Errores

--- main.py
+++ main.py
@@ -1,63 +1,61 @@
 import display.menu as menu
 from operaciones.basicas import suma, resta, multiplicacion, division
 from archivos.historial import guardar_operacion, mostrar_historial
+from utiles.funcionales import pedir_numero
 
 
 def main():
-    #historial = []
+    operaciones_validas = ['+', '-', '*', '/', 'h', '0'] 
     menu.clear()
     choice = '1'    
 
     while True:
-        choice = input('Ingrese la operación (+, -, *, /, h(historial)) o "0" para salir:  ')
+        try:
+            choice = input('Ingrese la operación (+, -, *, /, h(historial)) o "0" para salir:  ')
 
-        if choice == "h":
-            mostrar_historial()
-            continue 
+            if choice == "h":
+                mostrar_historial()
+                continue 
 
-        if choice == "0":
-            print("Salir")
-            break
+            if choice == "0":
+                print("Salir")
+                break
+            
+            if choice not in operaciones_validas:
+                raise Exception('Opción inválida, vuelva a intentarlo')  
 
-        num1 = float(input("Ingrese un numero: "))  
-        num2 = float(input("Ingrese el segundo numero: "))  
+            num1 = pedir_numero("Ingrese un numero: ") 
+            num2 = pedir_numero("Ingrese el segundo numero: ")
 
-        if choice == '+':
-            resultado = suma(num1, num2)
-            operacion = f"{num1} + {num2} = {resultado}"
-            guardar_operacion(operacion)
-            #historial.append(operacion) 
-            print("La suma es:", operacion)         
-        
-        elif choice == '-':
-            resultado = resta(num1, num2)
-            operacion = f"{num1} - {num2} = {resultado}"
-            guardar_operacion(operacion)
-            #historial.append(operacion) 
-            print("La resta es:", operacion)
-        
-        elif choice == '*':
-            resultado = multiplicacion(num1, num2)
-            operacion = f"{num1} * {num2} = {resultado}"
-            guardar_operacion(operacion)
-            #historial.append(operacion) 
-            print("La multiplicación es:", operacion)
-        
-        elif choice == '/':
-            if num2 != 0:  
+            if choice == '+':
+                resultado = suma(num1, num2)
+                operacion = f"{num1} + {num2} = {resultado}"
+                guardar_operacion(operacion)    
+                print("La suma es:", operacion)         
+            
+            elif choice == '-':
+                resultado = resta(num1, num2)
+                operacion = f"{num1} - {num2} = {resultado}"
+                guardar_operacion(operacion)
+                print("La resta es:", operacion)
+            
+            elif choice == '*':
+                resultado = multiplicacion(num1, num2)
+                operacion = f"{num1} * {num2} = {resultado}"
+                guardar_operacion(operacion)
+                print("La multiplicación es:", operacion)
+            
+            elif choice == '/':
+                if num2 == 0:
+                    raise ZeroDivisionError("No se puede dividir por 0")                
                 resultado = division(num1, num2)
                 operacion = f"{num1} / {num2} = {resultado}"
-                #historial.append(operacion) 
-                print("La división es:", operacion)
-            else:
-                print("Error: No se puede dividir entre cero.") 
-                
-        elif choice == "0":            
-            print('Salir')
-            break    
-        
-        else:
-            print('Opción inválida, vuelva a intentarlo')  
+                print("La división es:", operacion)                               
+               
+        except ZeroDivisionError as error:
+            print('Error: ', error)
+        except Exception as error:
+            print(error)
 
 
 if __name__ == "__main__":
--- utiles/funcionales.py
+++ utiles/funcionales.py
@@ -4,4 +4,11 @@ from datetime import datetime
 def obtener_fecha():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    
+
+def pedir_numero(mje):
+    valor = input(mje)
+    try:
+        return float(valor)
+    except ValueError:
+        print("Por favor ingrese un numero válido")
+        return pedir_numero(mje)
